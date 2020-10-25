6d5d305d169d0b619321d0043df7eeb349d37e75
LFMP <luizfellipe42@hotmail.com>
2020-10-24 23:31:49 -0300

    add sigmoid functions

---
+++ src/util/sigmoid.h
@@ -0,0 +1,35 @@
+#include <math.h>
+
+typedef struct Sigmoid {
+  float* inputRef = NULL;
+  float* output = NULL;
+  float* gradient = NULL;
+  int width = 0;
+} sigmoid;
+
+void create_sigmoid(sigmoid* s, int width) {
+  s->width = width;
+  s->output = (float*)malloc(width * sizeof(float));
+  s->gradient = (float*)malloc(width * sizeof(float));
+}
+
+float calc_sigmoid(float in) {
+  return 1.0 / (1.0 + exp(-1 * in));
+}
+
+float sigmoid_complement(float in) {
+  return calc_sigmoid(in) * (1.0 - calc_sigmoid(in));
+}
+
+void sigmoid_forward(sigmoid* in, float* vec_in) {
+  for (int i = 0; i < in->width; i++) {
+    in->output[i] = calc_sigmoid(vec_in[i]);
+  }
+  in->inputRef = vec_in;
+}
+
+void sigmoid_backward(sigmoid* in, float* in_gradient) {
+  for (int i = 0; i < in->width; i++) {
+    in->gradient[i] = sigmoid_complement(in->inputRef[i]) * in_gradient[i];
+  }
+}
