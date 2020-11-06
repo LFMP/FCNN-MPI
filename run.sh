cd489ef20c649196c6915864b048ff1a9e32edc4
LFMP <luizfellipe42@hotmail.com>
2020-11-06 16:19:37 -0300

    run changes in run.sh and makefiles

--- run.sh
+++ run.sh
@@ -18,46 +18,31 @@ SUMS2=0
 SUMS3=0
 SUMS4=0
 echo "executando sequencial"
-for ((i = 0; i < $N; ++i)); do
-  SUMS1=$(($SUMS1 + $(./$sequential -n 1000 -m 300 -d $DATASET)))
-done
+$PERF -r $N $RUNF ./$sequential -n 1000 -m 300 -d $DATASET >>sequential.1000.300.txt
 
-for ((i = 0; i < $N; ++i)); do
-  SUMS2=$(($SUMS2 + $(./$sequential -n 9000 -m 3000 -d $DATASET)))
-done
+$PERF -r $N $RUNF ./$sequential -n 9000 -m 3000 -d $DATASET >>sequential.9000.3000.txt
 
-for ((i = 0; i < $N; ++i)); do
-  SUMS3=$(($SUMS3 + $(./$sequential -n 30000 -m 6000 -d $DATASET)))
-done
+$PERF -r $N $RUNF ./$sequential -n 30000 -m 6000 -d $DATASET >>sequential.30000.6000.txt
 
-for ((i = 0; i < $N; ++i)); do
-  SUMS4=$(($SUMS4 + $(./$sequential -n 60000 -m 10000 -d $DATASET)))
-done
+$PERF -r $N $RUNF ./$sequential -n 60000 -m 10000 -d $DATASET >>sequential.60000.10000.txt
 
 # executa paralelo
 echo "executando paralelo"
 for k in 2 4 6 8; do
-  ((SUMP = 0))
-  for ((i = 0; i < $N; ++i)); do
-    ((SUMP = $(($SUMP + $(mpirun -np $k $parallel -n 1000 -m 300 -d $DATASET)))))
-  done
-  echo "print('Speedup para small e $k processos: ',($SUMS1/$N)/($SUMP/$N))" | python3
 
-  ((SUMP = 0))
   for ((i = 0; i < $N; ++i)); do
-    ((SUMP = $(($SUMP + $(mpirun -np $k $parallel -n 9000 -m 3000 -d $DATASET)))))
+    mpirun -N $k $parallel -n 1000 -m 300 -d $DATASET >>parallel.$k.1000.300.txt
   done
-  echo "print('Speedup para medium e $k processos: ',($SUMS2/$N)/($SUMP/$N))" | python3
 
-  ((SUMP = 0))
   for ((i = 0; i < $N; ++i)); do
-    ((SUMP = $(($SUMP + $(mpirun -np $k $parallel -n 30000 -m 6000 -d $DATASET)))))
+    mpirun -N $k $parallel -n 9000 -m 3000 -d $DATASET >>parallel.$k.9000.3000.txt
   done
-  echo "print('Speedup para large e $k processos: ',($SUMS3/$N)/($SUMP/$N))" | python3
 
-  ((SUMP = 0))
   for ((i = 0; i < $N; ++i)); do
-    ((SUMP = $(($SUMP + $(mpirun -np $k $parallel -n 60000 -m 10000 -d $DATASET)))))
+    mpirun -N $k $parallel -n 30000 -m 6000 -d $DATASET >>parallel.$k.30000.6000.txt
   done
-  echo "print('Speedup para extra large e $k processos: ',($SUMS4/$N)/($SUMP/$N))" | python3
+
+  for ((i = 0; i < $N; ++i)); do
+    mpirun -N $k $parallel -n 60000 -m 10000 -d $DATASET >>parallel.$k.60000.10000.txt
+  done
 done
