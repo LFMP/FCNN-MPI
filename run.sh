b9707def7cb0f2e9d03a25d31b78845300c0ee61
LFMP <luizfellipe42@hotmail.com>
2020-11-11 11:53:34 -0300

    remove debug configs

--- run.sh
+++ run.sh
@@ -4,8 +4,6 @@ DIR=$(pwd)
 N=$1
 parallel="src/parallel/fcnn-mpi.out"
 sequential="src/sequential/fcnn.out"
-PERF="sudo perf stat"
-RUNF="-a -B -e cache-misses,cache-references,cycles,instructions,mem-loads,mem-stores,context-switches,power/energy-cores/,power/energy-ram/,power/energy-pkg/"
 DATASET="$DIR/Datasets/dataset1/"
 if [ -z "$N" ]; then
   echo "Eh necessario informar a quantidade de execucoes"
@@ -18,31 +16,46 @@ SUMS2=0
 SUMS3=0
 SUMS4=0
 echo "executando sequencial"
-$PERF -r $N $RUNF ./$sequential -n 1000 -m 300 -d $DATASET >>sequential.1000.300.txt
+for ((i = 0; i < $N; ++i)); do
+  SUMS1=$(bc <<<$SUMS1 + $(./$sequential -n 1000 -m 300 -d $DATASET))
+done
 
-$PERF -r $N $RUNF ./$sequential -n 9000 -m 3000 -d $DATASET >>sequential.9000.3000.txt
+for ((i = 0; i < $N; ++i)); do
+  SUMS2=$(bc <<<$SUMS2 + $(./$sequential -n 9000 -m 3000 -d $DATASET))
+done
 
-$PERF -r $N $RUNF ./$sequential -n 30000 -m 6000 -d $DATASET >>sequential.30000.6000.txt
+for ((i = 0; i < $N; ++i)); do
+  SUMS3=$(bc <<<$SUMS3 + $(./$sequential -n 30000 -m 6000 -d $DATASET))
+done
 
-$PERF -r $N $RUNF ./$sequential -n 60000 -m 10000 -d $DATASET >>sequential.60000.10000.txt
+for ((i = 0; i < $N; ++i)); do
+  SUMS4=$(bc <<<$SUMS4 + $(./$sequential -n 60000 -m 10000 -d $DATASET))
+done
 
 # executa paralelo
 echo "executando paralelo"
-for k in 2 4; do
-
+for k in 2 3 4; do
+  ((SUMP = 0))
   for ((i = 0; i < $N; ++i)); do
-    mpirun -N $k $parallel -n 1000 -m 300 -d $DATASET >>parallel.$k.1000.300.txt
+    SUMP=$(bc <<<$SUMP + $(mpirun -np $k $parallel -n 1000 -m 300 -d $DATASET))
   done
+  echo "print('Speedup para small e $k processos: ',($SUMS1/$N)/($SUMP/$N))" | python3
 
+  ((SUMP = 0))
   for ((i = 0; i < $N; ++i)); do
-    mpirun -N $k $parallel -n 9000 -m 3000 -d $DATASET >>parallel.$k.9000.3000.txt
+    SUMP=$(bc <<<$SUMP + $(mpirun -np $k $parallel -n 9000 -m 3000 -d $DATASET))
   done
+  echo "print('Speedup para medium e $k processos: ',($SUMS2/$N)/($SUMP/$N))" | python3
 
+  ((SUMP = 0))
   for ((i = 0; i < $N; ++i)); do
-    mpirun -N $k $parallel -n 30000 -m 6000 -d $DATASET >>parallel.$k.30000.6000.txt
+    SUMP=$(bc <<<$SUMP + $(mpirun -np $k $parallel -n 30000 -m 6000 -d $DATASET))
   done
+  echo "print('Speedup para large e $k processos: ',($SUMS3/$N)/($SUMP/$N))" | python3
 
+  ((SUMP = 0))
   for ((i = 0; i < $N; ++i)); do
-    mpirun -N $k $parallel -n 60000 -m 10000 -d $DATASET >>parallel.$k.60000.10000.txt
+    SUMP=$(bc <<<$SUMP + $(mpirun -np $k $parallel -n 60000 -m 10000 -d $DATASET))
   done
+  echo "print('Speedup para extra large e $k processos: ',($SUMS4/$N)/($SUMP/$N))" | python3
 done
